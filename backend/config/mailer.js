e59a9695c5ccb269b808674e0e4b8a9ca3ebbf40
shubham pratap <shubhampratap9898@gmail.com>
2026-06-04 15:48:52 +0530

    Switch to SendGrid for email sending

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,10 +1,11 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: 'smtp.sendgrid.net',
+    port: 587,
     auth: {
-        user: process.env.APP_EMAIL,
-        pass: process.env.APP_EMAIL_PASSWORD,
+        user: 'apikey',
+        pass: process.env.SENDGRID_API_KEY,
     },
 });
 
